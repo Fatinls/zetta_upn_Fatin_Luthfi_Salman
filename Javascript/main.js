4875ee5a8efa8f2259188083f2825ada10eff355
Fatinls <91710776+Fatinls@users.noreply.github.com>
2023-03-14 19:36:21 +0700

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,24 @@
+function purchaseBook(title, author, price, discountPercentage, taxPercentage) {
+  const TAX_RATE = 0.1; // 10% tax rate
+  let discountedPrice = price * (1 - (discountPercentage / 100)); // calculate price after discount
+  let discountAmount = price - discountedPrice; // calculate amount of discount
+  let taxAmount = discountedPrice * (taxPercentage / 100); // calculate amount of tax
+  let totalPrice = discountedPrice + taxAmount; // calculate total price after tax
+  
+    
+
+  // display results
+  console.log(`Title: ${title}`);
+  console.log(`Author: ${author}`);
+  console.log(`Price: $${price.toFixed(2)}`);
+  console.log(`Discount: ${discountPercentage}%`);
+  console.log(`Discount amount: $${discountAmount.toFixed(2)}`);
+  console.log(`Price after discount: $${discountedPrice.toFixed(2)}`);
+  console.log(`Tax: ${taxPercentage}%`);
+  console.log(`Tax amount: $${taxAmount.toFixed(2)}`);
+  console.log(`Total price: $${totalPrice.toFixed(2)}`);
+  
+}
+
+// example usage
+purchaseBook("THE SUBTLE ART OF NOT GIVING A FUCK", "Mark Manson", 320000, 20, 8.5);
